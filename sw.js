4241024d1c7f5a3d05da061f0a9fdc9dc014df8f
Your Name <you@example.com>
2026-08-24 22:07:51 +0300

    fix: correct PropellerAds zoneId in sw.js (11645923 -> 11645958 Push Notifications)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11645923
+    "zoneId": 11645958
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
